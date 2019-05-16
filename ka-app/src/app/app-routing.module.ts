15df2972f3ca0f3608f130d4e0ada6dd0a1edfce
mad <madhu.julian94@gmail.com>
2019-05-16 14:23:26 +0530

    commited maste component and sidebar

--- ka-app/src/app/app-routing.module.ts
+++ ka-app/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MasterComponent } from './master/master.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+	{ path: 'master', component: MasterComponent },
+	{ path: '',   redirectTo: '/master', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
